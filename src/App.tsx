4a90f2a43fadf5f43061e43bca411fc74a698e12
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-03 22:34:38 +0900

    Add Routes Home Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button
-                    type="button"
-                    onClick={() => setCount((prev) => prev + 1)}
-                >
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './common/GlobalStyle';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <GlobalStyle />
-        <App />
+        <BrowserRouter>
+            <GlobalStyle />
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
 );
