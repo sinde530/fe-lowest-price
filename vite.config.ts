8469fc2fcca3aba7d0abe7fbf49ac2d0122d8f2d
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-04 13:44:12 +0900

    Add mobile server host `*`

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  }
 })
